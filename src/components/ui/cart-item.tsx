e36d54afbb8ee58d5fb533b5388fdc152bd6f688
EsdrasCFF <ecff.dev@gmail.com>
2023-11-15 18:06:23 -0300

    feat: add CartItem component and improved cart product addition

---
+++ src/components/ui/cart-item.tsx
@@ -0,0 +1,61 @@
+import { CartProduct } from "@/contexts/cart";
+import { priceFormatter } from "@/helpers/formatter";
+import Image from 'next/image'
+import { Button } from "./button";
+import { ArrowLeft, ArrowRight, TrashIcon } from "lucide-react";
+
+interface CartItemProps {
+  product: CartProduct;
+}
+
+export function CartItem({product}: CartItemProps) {
+  return(
+    <div className="flex items-center justify-between" >
+      <div className="flex items-center gap-4" >
+        <div className="flex items-center justify-center rounded-lg bg-accent h-[77px] w-[77px]" >
+          <Image
+            src={product.imageUrls[0]}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt={product.name}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          />
+        </div>
+
+        <div className="flex flex-col" >
+          <p className="text-xs" >{product.name}</p>
+
+          <div className="flex items-center gap-2" >
+            <p className="text-sm font-bold">
+              { priceFormatter.format(product.totalPrice)}
+            </p>
+            
+            {product.discountPercentage > 0 && (
+              <p className="text-xs line-through opacity-60">
+                {priceFormatter.format(Number(product.basePrice))}
+              </p>
+            )}
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Button size="icon" variant="outline" className="h-8 w-8">
+              <ArrowLeft size={16} />
+            </Button>
+
+            <span className="text-xs" > {product.quantity} </span>
+
+            <Button size="icon" variant="outline" className="h-8 w-8">
+              <ArrowRight size={16} />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      
+      <Button variant="outline" >
+        <TrashIcon size={16}/>
+      </Button>
+    </div>
+  )
+}
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,13 +2,15 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react"
 import { CartContext } from "@/contexts/cart";
+import { CartItem } from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 export function Cart() {
   const { products } = useContext(CartContext)
 
 
   return (
-    <div>
+    <div className="flex flex-col gap-8" >
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 text-base uppercase py-[0.375rem]" 
         variant="outline" 
@@ -18,9 +20,11 @@ export function Cart() {
       </Badge>
 
       {/* RENDERIZAR OS PRODUTOS */}
-      {products.map((product) => (
-        <h1 key={product.id}> {product.name} </h1>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem key={product.id} product={computeProductTotalPrice(product as any) as any} />
+        ))}
+      </div>
     </div>
   )
 }
