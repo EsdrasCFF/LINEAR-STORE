232ef0978ff5566778f661ae7adfe65129b8290a
EsdrasCFF <ecff.dev@gmail.com>
2023-12-03 17:42:51 -0300

    chore: show formatter date

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -8,3 +8,11 @@ export const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
   month: '2-digit',
   year: 'numeric'
 });
+
+export const dateAndHourFormatter = new Intl.DateTimeFormat('pt-BR', {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit'
+});
